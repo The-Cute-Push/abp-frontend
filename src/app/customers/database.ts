3e9c573838ed9419adbcf12ced8afc0a976de59f
rafaelgarciaderocchi <rafaelgarciaderocchi@icloud.com>
2025-06-02 21:45:37 -0300

    Estrutura do componente table criada, feito banco de dados temporarios

---
+++ src/app/customers/database.ts
@@ -0,0 +1,5 @@
+export const Customers = {
+    customers: [
+        { id: "1", name: "Jorge", cpf: "231.345.234-21", createdAt: "02/06/2025", updatedAt: "02/06/2025" },
+    ]
+}
---
+++ src/app/products/database.ts
@@ -0,0 +1,5 @@
+export const Products = {
+    products: [
+        { id: "1", name: "Banana", code: "8A83D45GCDNJ", price: "20", cost: "10", quantity: "4", photo: "url", createdAt: "02/06/2025", updatedAt: "02/06/2025" },
+    ]
+}
