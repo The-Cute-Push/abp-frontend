5018d00a2b84b074326a25b12f49199016a4634a
Lucas De Bitencourt Frasson <lucasb.frasson@gmail.com>
2025-06-07 20:31:41 -0300

    feat/login: add login component with HTML and CSS styles + global arial font

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CustomersComponent } from './customers/customers.component';
 import { ProductsComponent } from './products/products.component';
 import { SuppliersComponent } from './suppliers/suppliers.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: "customers", component: CustomersComponent},
     {path: "products", component: ProductsComponent},
     {path: "suppliers", component: SuppliersComponent},
+    {path: "login", component: LoginComponent}
 ];
