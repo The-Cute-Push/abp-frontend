fe968978fae93fb38bceeaabb7aeb1fe58f141d9
unknown <brendoncordova13@gmail.com>
2025-06-20 22:39:31 -0300

    Brendon - Fazendo o login funcionar(adm@gmail.com) senha: 1234. E mudando a cor dos hovers para azul

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,26 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-    selector: "app-login",
-    templateUrl: "./login.component.html",
-    styleUrls: ["./login.component.css"]
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email = '';
+  password = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.email === 'adm@gmail.com' && this.password === '1234') {
+      this.router.navigate(['/home']);
+    } else {
+      alert('Usuário ou senha inválidos.');
+    }
+  }
 }
